621cf24f1c2578364fde7cf0e96b2dd4b3a3f2a2
luwam Habtu <35058183-luwamhabtu07@users.noreply.replit.com>
2024-08-15 21:08:43 +0000

    Updated MongoDB connection string and fixed Express setup

--- 3_DB/index.js
+++ 3_DB/index.js
@@ -1,23 +1,33 @@
-'use strict'
-
 import express from 'express';
-import handlebars from "express-handlebars"
-import { Book } from "../models/book.js";
+import { engine } from 'express-handlebars';
+import mongoose from 'mongoose';
+import { Book } from './models/book.js';
 
 const app = express();
 
 app.set("port", process.env.PORT || 3000);
-app.use(express.static('./public')); // allows direct navigation to static files
-app.use(express.urlencoded()); //Parse URL-encoded bodies
-app.use(express.json()); //Used to parse JSON bodies
+app.use(express.static('./public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 import cors from 'cors';
-app.use('/api', cors()); // set Access-Control-Allow-Origin header for api route
+app.use('/api', cors());
 
-app.engine('hbs', handlebars({defaultLayout: "main.hbs"}));
+app.engine('hbs', engine({
+    defaultLayout: "main.hbs",
+    extname: '.hbs'
+}));
 app.set("view engine", "hbs");
 
-app.get('/', (req,res) => {
+// MongoDB connection
+mongoose.connect('mongodb+srv://<username>:<password>@cluster0.mongodb.net/sccproject', {
+    // No deprecated options
+})
+.then(() => console.log('Mongoose connected.'))
+.catch(err => console.error('Error connecting to MongoDB:', err));
+
+// Routes
+app.get('/', (req, res, next) => {
     Book.find({}).lean()
         .then((books) => {
             res.render('home', { books });
@@ -25,45 +35,47 @@ app.get('/', (req,res) => {
         .catch(err => next(err));
 });
 
-app.get('/about', (req,res) => {
+app.get('/about', (req, res) => {
     res.type('text/html');
     res.render('about');
 });
 
-app.get('/detail', (req,res,next) => {
-    Book.findOne({ title:req.query.title }).lean()
+app.get('/detail', (req, res, next) => {
+    Book.findOne({ title: req.query.title }).lean()
         .then((book) => {
-            res.render('details', {result: book} );
+            res.render('details', { result: book });
         })
         .catch(err => next(err));
 });
 
-app.post('/detail', (req,res, next) => {
-    Book.findOne({ title:req.body.title }).lean()
+app.post('/detail', (req, res, next) => {
+    Book.findOne({ title: req.body.title }).lean()
         .then((book) => {
-            res.render('details', {result: book} );
+            res.render('details', { result: book });
         })
         .catch(err => next(err));
 });
 
-app.get('/delete', (req,res) => {
-    Book.remove({ title:req.query.title }, (err, result) => {
-        if (err) return next(err);
-        let deleted = result.result.n !== 0; // n will be 0 if no docs deleted
-        Book.count((err, total) => {
+app.get('/delete', (req, res, next) => {
+    Book.deleteOne({ title: req.query.title })
+        .then(result => {
+            let deleted = result.deletedCount !== 0;
+            return Book.countDocuments();
+        })
+        .then(total => {
             res.type('text/html');
-            res.render('delete', {title: req.query.title, deleted: result.result.n !== 0, total: total } );    
-        });
-    });
+            res.render('delete', { title: req.query.title, deleted: deleted, total: total });
+        })
+        .catch(err => next(err));
 });
 
-app.use((req,res) => {
-    res.type('text/plain'); 
+// 404 handler
+app.use((req, res) => {
+    res.type('text/plain');
     res.status(404);
     res.send('404 - Not found');
 });
 
 app.listen(app.get('port'), () => {
-    console.log('Express started');    
+    console.log('Express started');
 });
-
